3538aabf48c01072b5cb58804dfaf3df22c59f77
Maciej 'Matt' Madejsza <maciej.madejsza@gmail.com>
2021-07-28 20:24:17 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
+import { render } from '@testing-library/react';
 import React, { Component } from 'react';
 import './App.css';
 import Comment from './Components/Comment.js';
 
-function App() {
-  function randomPic() {
-    return
-  }
+class App extends Component {
 
-  return (
-    <div>
-      <h1>Today's voice:</h1>
-      <div className="App">
-        {/* muszę ręcznie przeładować hosta bo inaczej nie widać zmian w propsach. w reszcie rzeczy widać 
+
+  randomPic = () => {
+    return ""
+  }
+  render() {
+    return (
+      <div>
+        <h1>Today's voice:</h1>
+        <div className="App">
+          {/* muszę ręcznie przeładować hosta bo inaczej nie widać zmian w propsach. w reszcie rzeczy widać 
         
         Jak chcę teraz zmienić nazwy propsów np z color na colorHeader i colorContent to ręcznie musze wszystko zmienić w każdym komponencie?
 
@@ -21,38 +24,40 @@ function App() {
         
         */}
 
-        <Comment
-          pic={"https://www.usedairsoft.co.uk/oc-content/uploads/264/57119.jpg"}
-          color={"red"}
-          fontSize={"20px"}
-          fontWeight={"700"}
-          headerJustify={"center"}
-          contentJustify={"right"} />
-        <Comment
-          pic={"https://www.usedairsoft.co.uk/oc-content/uploads/264/57119.jpg"}
-          color={"green"}
-          fontSize={"14px"}
-          fontWeight={"400"}
-          headerJustify={"left"}
-          contentJustify={"center"} />
-        <Comment
-          pic={"https://www.usedairsoft.co.uk/oc-content/uploads/264/57119.jpg"}
-          color={"magenta"}
-          fontSize={"10px"}
-          fontWeight={"200"}
-          headerJustify={"right"}
-          contentJustify={"left"} />
-        <Comment
-          pic={this.randomPic}
-          color={""}
-          fontSize={""}
-          fontWeight={""}
-          headerJustify={""}
-          contentJustify={""} />
+          <Comment
+            pic={"https://www.usedairsoft.co.uk/oc-content/uploads/264/57119.jpg"}
+            color={"red"}
+            fontSize={"20px"}
+            fontWeight={"700"}
+            headerJustify={"center"}
+            contentJustify={"right"} />
+          <Comment
+            pic={"https://www.usedairsoft.co.uk/oc-content/uploads/264/57119.jpg"}
+            color={"green"}
+            fontSize={"14px"}
+            fontWeight={"400"}
+            headerJustify={"left"}
+            contentJustify={"center"} />
+          <Comment
+            pic={"https://www.usedairsoft.co.uk/oc-content/uploads/264/57119.jpg"}
+            color={"magenta"}
+            fontSize={"10px"}
+            fontWeight={"200"}
+            headerJustify={"right"}
+            contentJustify={"left"} />
+          <Comment
+            pic={this.randomPic}
+            color={""}
+            fontSize={""}
+            fontWeight={""}
+            headerJustify={""}
+            contentJustify={""} />
+        </div >
       </div >
-    </div >
-  );
+    );
+  }
 }
+
 
 
 
