14bc6cae796e4dfb8868851b286cc00ad2687b4c
Maciej 'Matt' Madejsza <maciej.madejsza@gmail.com>
2021-07-28 21:42:02 +0100

    +1

--- src/Components/Comment.js
+++ src/Components/Comment.js
@@ -3,25 +3,12 @@ import CommentPic from '../Components/CommentPic.js'
 import CommentArticle from '../Components/CommentArticle.js'
 import "../CSS/Comment.css"
 
-//czy tu można użyć takich skrótów np.?
-//const store = this.props.store;
-// const form = this.props.form;
-// const loading = this.props.loading;
-// const errors = this.props.errors;
-// const entity = this.props.entity;
-// Shorthand:
-//import { observable, action, runInAction } from 'mobx';
-//const { store, form, loading, errors, entity } = this.props;
-//You can even assign your own variable names:
-// const { store, form, loading, errors, entity:contact } = this.props;
-
-
 class Comment extends Component {
     constructor(props) {
         super(props);
         this.state = {
             deleted: this.props.hidden,
-            pic: this.props.pic, //czy tu musi być this?
+            pic: this.props.pic,
             picAlt: "Icon",
             color: this.props.color,
             fontSize: this.props.fontSize,
@@ -32,12 +19,6 @@ class Comment extends Component {
     }
 
     deleteComment = () => {
-        // console.log('clicked');
-        // this.setState({
-        //     deleted: true
-        // })
-        console.log(this.props.hidden);
-
         this.props.deleted()
     }
 
